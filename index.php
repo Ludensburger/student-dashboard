7dbd6c814a1c773952e44685a1d3b37df3dd51ac
jacocanete <jacogagarincanete@gmail.com>
2024-12-18 11:01:36 +0800

    Remove old SQL schema and refactor index.php to include setup file

--- index.php
+++ index.php
@@ -1,17 +1,4 @@
 <?php
-/**
- * Iterate through php files in the includes folder and include them.
- */
+define ('ROOT', __DIR__);
 
-try {
-    $dir_iterator = new RecursiveDirectoryIterator(__DIR__ . '/includes/');
-    $it = new RecursiveIteratorIterator($dir_iterator);
-
-    foreach ($it as $file) {
-        if ($file->isFile() && $file->isReadable() && $file->getExtension() === 'php') {
-            require $file->getPathname();
-        }
-    }
-} catch (Exception $e) {
-    throw $e;
-}
+require_once ROOT . '/includes/setup.php';
