1f3539d9a25abdfc95d3a2752e4375a8acb2e5bf
jacocanete <jacogagarincanete@gmail.com>
2024-12-18 11:01:41 +0800

    Add login and registration modals to footer and update header for session management

--- components/footer.php
+++ components/footer.php
@@ -1,3 +1,46 @@
 <?php enqueue_scripts(); ?>
+
+<!-- login modal -->
+<div class="modal fade" id="login" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content p-3">
+      <form id="login-form">
+        <div class="mb-3">
+          <label for="usernamelogin" class="form-label">Username</label>
+          <input type="text" class="form-control" id="usernamelogin" name="username" required>
+        </div>
+        <div class="mb-3">
+          <label for="passwordlogin" class="form-label">Password</label>
+          <input type="password" class="form-control" id="passwordlogin" name="password" required>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  </div>
+</div>
+
+<!-- register modal -->
+<div class="modal fade" id="register" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content p-3">
+      <form id="register-form">
+        <div class="mb-3">
+          <label for="usernameregister" class="form-label">Username</label>
+          <input type="text" class="form-control" id="usernameregister" name="username" required>
+        </div>
+        <div class="mb-3">
+          <label for="passwordregister" class="form-label">Password</label>
+          <input type="password" class="form-control" id="passwordregister" name="password" required>
+        </div>
+        <div class="mb-3">
+          <label for="passwordregisterconfirm" class="form-label">Confirm Password</label>
+          <input type="password" class="form-control" id="passwordregisterconfirm" name="confirm_password" required>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  </div>
+</div>
+
 </body>
 </html>
--- components/header.php
+++ components/header.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,3 +10,30 @@
     <?php enqueue_styles(); ?>
 </head>
 <body>
+
+<nav class="navbar bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand"><strong>Student Masterlist</strong></a>
+    <p class="navbar__title"><?= !empty($_SESSION) ? 'Logged in as: ' . $_SESSION['user']['username'] : '' ?></p>
+    <div class="row">
+      <?php
+      if (! isset($_SESSION['user'])) {
+        ?>
+        <div class="col">
+          <button class="btn btn-outline-success login" data-bs-toggle="modal" data-bs-target="#login">Login</button>
+        </div>
+        <div class="col">
+          <button class="btn btn-outline-warning register" data-bs-toggle="modal" data-bs-target="#register">Register</button>
+        </div>
+        <?php
+      } else {
+        ?>
+        <div class="col-6">
+          <button class="btn btn-outline-danger logout">Logout</button>
+        </div>
+        <?php
+      }
+      ?>
+    </div>
+  </div>
+</nav>
